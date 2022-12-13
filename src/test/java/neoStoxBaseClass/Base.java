4ece442c86e9987fe381c7404bd54ce037bbd035
MonikaGathe <120001585+MonikaGathe@users.noreply.github.com>
2022-12-13 16:19:41 +0530

    Update Base.java

--- src/test/java/neoStoxBaseClass/Base.java
+++ src/test/java/neoStoxBaseClass/Base.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import neoStoxUtilityClass.Utility;
 
 
 public class Base 
 {    
 	// branch code
+	
 	protected static WebDriver  driver ;
  
 	public void lonchBrowser() throws IOException, InterruptedException
